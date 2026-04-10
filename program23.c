949680daf2d3384073e0852a80b5952de00d2daf
Pratibha Sagar Pawar <pratibhapowar1@gmail.com>
2026-04-10 19:51:03 +0530

    Display even numbers

---
+++ program23.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 2; iCnt<= iNo; iCnt=(iCnt+2))
+    {
+        printf("%d\t",iCnt);
+
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the frequancy : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+
+}
---
+++ program25.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 2; iCnt<= iNo; iCnt++)   
+    {
+        if(iCnt%2 == 0)
+        {
+              printf("%d\t",iCnt);
+        }
+      
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the frequancy : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+
+}
